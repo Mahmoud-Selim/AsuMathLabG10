0f005fa732fed49005210fc8c9b8137b94e7c406
Mahmoud Ahmed Selim <mahmoudahmedselim96@gmail.com>
2018-01-30 08:28:19 +0200

    Update linked.cpp

--- linked.cpp
+++ linked.cpp
@@ -9,81 +9,64 @@
 
 
 
-node *head = NULL;//pointer to the first structure in list
-node *current = NULL;//pointer to the current structure in list
+node *head = NULL;
+node *current = NULL;
 
 
-	//Create Linked List
-	CMatrix * INSERT(string ID,int Nrows,int Ncolumns)
+CMatrix * INSERT(string ID,int Nrows,int Ncolumns)
+{
+	node *link = new node;
+	link->Matrix =new CMatrix(Nrows,Ncolumns);
+	link->ID = ID;
+	link->next = NULL;
+	if (head == NULL)
 	{
-		// Allocate memory for new node;
-		node *link = new node;
-		link->Matrix =new CMatrix(Nrows,Ncolumns);
-		link->ID = ID;
-		link->next = NULL;
-
-		// If head is empty, create new list
-		if (head == NULL)
-		{
-			head = link;
-			return link->Matrix;
-
-		}
-
-		current = head;
-
-		// move to the end of the list
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-
-		// Insert link at the end of the list
-		current->next = link;
+		head = link;
 		return link->Matrix;
-
 	}
-	/*
-	 *
-	 * ISEXIST Function
-	 *
-	 */
-	CMatrix* ISEXISTING (string id)
+	current = head;
+	while (current->next != NULL)
 	{
-		   if(head==NULL)
-		   {
-		    //cout <<("Linked List is empty");
-		      return NULL;
-		   }
-
-		   // start checking from the first node
-		   current = head;
-		   while(current != NULL)
-		   {
-		      if(current->ID == id)
-		      {
-		    	  //cout<<current->ID<<endl;
-		    	  return current ->Matrix;
-
-		      }
-		      current = current->next;
-
-		   }
-		   return NULL;
-
+		current = current->next;
 	}
+	current->next = link;
+	return link->Matrix;
+}
 
-	void KILLLIST(void)
+
+CMatrix* ISEXISTING (string id)
+{
+	if(head==NULL)
+	{
+		return NULL;
+	}
+	current = head;
+	while(current != NULL)
+	{
+		if(current->ID == id)
 		{
-			current = head;
-			if(head->next==NULL && head != NULL)//list is empty
-			{
-				delete head->Matrix;
-				return ;
-			}
-			while(current->next!=NULL)
-			{
-				delete current->Matrix;
-				current=current->next;
-			}
+			return current ->Matrix;
 		}
+		current = current->next;
+	}
+	return NULL;
+}
+
+
+void KILLLIST(void)
+{
+	current = head;
+	if(head != NULL)
+	{
+		if(head->next==NULL)//list is empty
+		{
+			delete head->Matrix;
+			return ;
+		}
+		while(current->next!=NULL)
+		{
+			delete current->Matrix;
+				current=current->next;
+		}
+	}
+}
